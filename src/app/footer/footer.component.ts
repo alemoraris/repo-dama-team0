31459bf4c8e4d932d5af4ccd37a85964348cd7e5
Alejandro <alejandro.morenoaristizabal@cieep.com>
2024-04-19 10:54:08 +0200

    Home,navbar,footer

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-footer',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css'
+})
+export class FooterComponent {
+
+}
